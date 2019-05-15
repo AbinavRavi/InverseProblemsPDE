3cee154e1848046e538900411df68bb0234fb9d9
AbinavRavi <abinav.ravi@tum.de>
2019-05-15 15:00:19 +0200

    added basic tutorial

---
+++ FenicsTutorials/poisson.py
@@ -0,0 +1,37 @@
+from fenics import *
+import matplotlib.pyplot as plt
+
+
+#create mesh and define function space
+mesh = UnitSquareMesh(16,16)
+V = FunctionSpace(mesh,'P',3)
+
+#define boundary conditions
+u_D = Expression('1+x[0]*x[0]+ 2*x[1]*x[1]',degree=2)
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V,u_D,boundary)
+
+#define the variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Constant(0.0)
+a = dot(grad(u),grad(v))*dx
+L = f*v*dx
+
+#compute solution
+u = Function(V)
+solve(a == L, u,bc)
+
+#plot solution and mesh
+plot(u)
+plot(mesh)
+
+#compute the error in L2 norm
+error_L2 = errornorm(u_D, u, 'L2')
+
+#print error
+print('error_L2 = ',error_L2)
+plt.show()
