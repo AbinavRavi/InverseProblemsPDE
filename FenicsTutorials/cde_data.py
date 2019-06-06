f7f9d6366b7210a520ee08e5279bf31e08d80723
AbinavRavi <abinav.ravi@tum.de>
2019-06-06 14:35:24 +0200

    added some parts with conditions from PDE net

---
+++ FenicsTutorials/cde_data.py
@@ -0,0 +1,24 @@
+from dolfin import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+T = 5.0            # final time
+num_steps = 500   # number of time steps
+dt = T / num_steps # time step size
+nx = ny = 50
+
+#define the mesh
+mesh = UnitSquareMesh(nx,ny)
+V = FunctionSpace(mesh)
+
+#define boundary conditions
+u_D = Expression('for(int i=0;i<9;i++){std::normal_distribution<double> distribution(0.0,0.02)*(cos(x[0]+x[1])+sin(x[0]+x[1]))}')
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V,u_D,boundary)
+
+u = TrialFunction(V)
+v = TestFunction(V)
+
