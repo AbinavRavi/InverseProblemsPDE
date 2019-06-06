813c04879f14ced47da466110df7d5906fe6da85
AbinavRavi <abinav.ravi@tum.de>
2019-06-06 16:00:12 +0200

    buggy code some parts not working yet

--- FenicsTutorials/cde_data.py
+++ FenicsTutorials/cde_data.py
@@ -9,23 +9,46 @@ nx = ny = 50
 
 #define the mesh
 mesh = UnitSquareMesh(nx,ny)
-V = FunctionSpace(mesh)
+V = VectorFunctionSpace(mesh,'P',1)
 
 #define boundary conditions
-u_D = Expression('for(int i=0;i<9;i++){std::normal_distribution<double> distribution(0.0,0.02)*(cos(x[0]+x[1])+sin(x[0]+x[1]))}')
+u_D = Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
 
 def boundary(x, on_boundary):
     return on_boundary
 
 bc = DirichletBC(V,u_D,boundary)
-
+# (u0,u1,u2) = split(Function(V))
 u = TrialFunction(V)
+# (v1,v2,v3) = TestFunctions(V)
 v = TestFunction(V)
 f = Constant(0.0)
-
+u_n = Function(V)
+w =Function(V)
+# + dot(w,grad(u))*v*dx 
 #define the variational problem
-F = ((u-u_n)/dt)*v*dx + dot(w,grad(u))*v*dx + dot(grad(u),grad(v))*dx - dot(f,v)*dx
+a = ((u-u_n)/dt)*v*dx + dot(grad(u),grad(v))*dx
+# a = lhs(F)
+L = f*v*dx
 
 #solve the variational problem
 u = Function(V)
-solve(a == L,u,bc)
+# solve(a == L,u,bc)
+
+#time stepping of the problem
+t = 0
+
+for n in range(num_steps):
+    t+=dt
+    b = assemble(L)
+    bc.apply(b)
+    solve(a == L,u,bc)
+    plt.figure(n)
+    plot(u)
+    plt.show()
+
+def write(u,nx,ny):
+    a = np.reshape(u.compute_vertex_values(),[nx+1,ny+1])
+    np.savetxt("sol_cda.csv",a,delimiter=",")
+
+write(u,nx,ny)
