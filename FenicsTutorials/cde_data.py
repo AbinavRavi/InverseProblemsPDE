3a734a79bebac57b9eb4406e95adb75f9b008dd9
AbinavRavi <abinav.ravi@tum.de>
2019-06-08 00:07:13 +0200

    daily update

--- FenicsTutorials/cde_data.py
+++ FenicsTutorials/cde_data.py
@@ -9,23 +9,21 @@ nx = ny = 50
 
 #define the mesh
 mesh = UnitSquareMesh(nx,ny)
-V = VectorFunctionSpace(mesh,'P',1)
+V = FunctionSpace(mesh,'P',1)
 
 #define boundary conditions
 u_D = Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
 
 def boundary(x, on_boundary):
-    return on_boundary
+    return near(x[0], 0)
 
 bc = DirichletBC(V,u_D,boundary)
-# (u0,u1,u2) = split(Function(V))
 u = TrialFunction(V)
-# (v1,v2,v3) = TestFunctions(V)
 v = TestFunction(V)
 f = Constant(0.0)
 u_n = Function(V)
-w =Function(V)
-# + dot(w,grad(u))*v*dx 
+w = Function(V)
+# + dot(w,grad(u))*v*dx
 #define the variational problem
 a = ((u-u_n)/dt)*v*dx + dot(grad(u),grad(v))*dx
 # a = lhs(F)
@@ -34,7 +32,7 @@ L = f*v*dx
 #solve the variational problem
 u = Function(V)
 # solve(a == L,u,bc)
-
+timeseries = TimeSeries('time')
 #time stepping of the problem
 t = 0
 
