8cde120060bc1810f067a7707c48e357a511a1fe
AbinavRavi <abinav.ravi@tum.de>
2019-06-10 11:49:09 +0200

    working code but without boundary conditions

--- cde_trial.py
+++ cde_trial.py
@@ -15,22 +15,54 @@ V = FunctionSpace(mesh,P)
 W = VectorFunctionSpace(mesh,'P',1)
 
 # set the boundary conditions
-u_D = Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
-def boundary(x, on_boundary):
-    return on_boundary
+# u_D = Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
+# def boundary(x, on_boundary):
+#     return on_boundary
 
-bc = DirichletBC(V,u_D,boundary)
+# bc = DirichletBC(V,u_D,boundary)
 
 # set the test and trial functions
 u = TrialFunction(V)
 v = TestFunction(V)
 w = Function(W)
+u = Function(V)
 u_n = Function(V)
 f = Constant(0.0)
 F = ((u - u_n) /dt)*v*dx + dot(w, grad(u))*v*dx + dot(grad(u), grad(v))*dx - f*v*dx 
-a = lhs(F)
-L = rhs(F)
+# a = lhs(F)
+# L = rhs(F)
+# u = Function(V)
 
+#create time series
+timeseries = TimeSeries('timeseries_cde')
 
+t = 0
+for n in range(num_steps):
 
+    # Update current time
+    t += dt
 
+    # b = assemble(lhs(F))
+    # bc.apply(b)
+
+    # Read velocity from file
+    timeseries.store(w.vector(), t)
+
+    # Solve variational problem for time step
+    solve(F == 0, u)
+
+    # Save solution to file (VTK)
+    # _u_1, _u_2, _u_3 = u.split()
+    # vtkfile_u_1 << (_u_1, t)
+    # vtkfile_u_2 << (_u_2, t)
+    # vtkfile_u_3 << (_u_3, t)
+
+    # Update previous solution
+    u_n.assign(u)
+    plot(u)
+
+    # Update progress bar
+    # progress.update(t / T)
+
+# Hold plot
+plt.show()
