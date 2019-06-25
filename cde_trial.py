a7c2e397596975408cfd33e5b0c6087bdc6a0615
AbinavRavi <abinav.ravi@tum.de>
2019-06-25 17:18:44 +0200

    added final simulation script

--- cde_trial.py
+++ cde_trial.py
@@ -6,7 +6,7 @@ import numpy as np
 T = 10
 num_steps = 100
 dt = T/num_steps
-nx = ny = 10
+nx = ny = 64
 
 # set the mesh and geometry
 P = FiniteElement('P',triangle,1)
@@ -14,8 +14,28 @@ mesh = UnitSquareMesh(nx,ny)
 V = FunctionSpace(mesh,P)
 W = VectorFunctionSpace(mesh,'P',1)
 
+#set the boundary conditions as random initialisations
+def U0xy(V):
+    u_n = interpolate(Constant(0), V)
+
+    #random distribution
+    mu, sigma = 0, 0.02 # mean and standard deviation
+
+    for m in range(9):
+        for n in range(9):
+
+            #initial value
+            u_0 = Expression('(rand_lambda*cos(k*x[0]+l*x[1]) + rand_gamma*sin(k*x[0]+l*x[1]))',degree=5,
+                             k=np.random.uniform(0,9), l=np.random.uniform(0,9),rand_lambda = np.random.normal(mu, sigma),
+                             rand_gamma = np.random.normal(mu, sigma))
+
+            u_t = interpolate(u_0, V)
+
+            u_n.vector().set_local(u_t.vector().get_local()+u_n.vector().get_local())
+    return u_n
+
 # set the boundary conditions 
-u_D = Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
+u_D = U0xy(V) #Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
 def boundary(x, on_boundary):
     return on_boundary
 
@@ -38,7 +58,7 @@ timeseries = TimeSeries('timeseries_cde')
 # Function to store data in np.array
 def write(u,nx,ny,timestep):
     a = np.reshape(u.compute_vertex_values(),[nx+1,ny+1])
-    np.savetxt('results/'+ str(timestep)+".csv",a,delimiter=",")
+    np.savetxt('results/64grid/'+ str(timestep)+".csv",a,delimiter=",")
 
 t = 0
 for n in range(num_steps):
