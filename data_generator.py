ca3b6a65e2733d1022d3c52610acd977b233503a
AbinavRavi <abinav.ravi@tum.de>
2019-07-12 09:24:01 +0200

    added the new data generator

--- data_generator.py
+++ data_generator.py
@@ -29,12 +29,15 @@ def boundary(x, on_boundary):
     return on_boundary
 
 # function to write the simulated quantities in a csv file
-def write(u,array,nx,ny,timestep,num_steps,sim_number):
+def write_function_values(u,nx,ny,timestep,num_steps,sim_number):
     a = np.reshape(u.compute_vertex_values(),[nx+1,ny+1])
-    if not os.path.exists('results/'+str(sim_number)+'/'):
-        os.makedirs('results/'+str(sim_number)+'/')
-    np.savetxt('results/'+str(sim_number)+'/'+str(timestep)+".csv",a,delimiter=",")
-    np.savetxt('results/'+str(sim_number)+'/'+str(timestep)+'_parameters'+".csv",array,delimiter=",")
+    if not os.path.exists('data/'+str(sim_number)+'/'):
+        os.makedirs('data/'+str(sim_number)+'/')
+    np.savetxt('data/'+str(sim_number)+'/'+str(timestep)+".csv",a,delimiter=",")
+    
+
+def write_parameters(array,sim_number):
+    np.savetxt('data/'+str(sim_number)+'_parameters'+".csv",array,delimiter=",")
 
     
 def Simulation(nx,ny,T,num_steps,sim_number):
@@ -42,50 +45,56 @@ def Simulation(nx,ny,T,num_steps,sim_number):
     P = FiniteElement('P',triangle,2)
     mesh = UnitSquareMesh(nx,ny)
     V = FunctionSpace(mesh,P)
-    W = VectorFunctionSpace(mesh,'P',2)
-
+    # W = VectorFunctionSpace(mesh,'P',2)
+    w_lower = Expression('-2.0',degree=2)
+    w_upper = Expression('2.0',degree=2)
+    eps_lower = Expression('0.2',degree=6)
+    eps_upper = Expression('0.8',degree=6)
     u_D = U0xy(V) #Expression('cos(x[0]+x[1])+sin(x[0]+x[1])',degree=1)
     bc = DirichletBC(V,u_D,boundary)
 
     # set the test and trial functions
     u = TrialFunction(V)
     v = TestFunction(V)
-    w = Function(W)
+    # w = Function(W)
     u = Function(V)
     u_n = Function(V)
     f = Constant(0.0)
-    k = Constant(np.random.uniform(-2,2))
-    eps = Constant(random.uniform(0.2,0.8))
-    F = ((u - u_n) /dt)*v*dx + k*dot(w, grad(u))*v*dx + eps*dot(grad(u), grad(v))*dx - f*v*dx 
-    array = [k,eps]
+    # k = Constant(np.random.uniform(-2,2))
+    # eps = Constant(random.uniform(0.2,0.8))
+    w = as_vector([w_lower,w_upper])
+    eps = as_matrix([[eps_lower,0],[0,eps_upper]])
+    DiffTerm=dot(dot(grad(u),eps),grad(v))*dx
+    Convterm=dot(grad(u),w)*v*dx
+    F=(u-u_n)*v*dx + dt*DiffTerm + dt*Convterm
+    # F = ((u - u_n) /dt)*v*dx + k*dot(w, grad(u))*v*dx + eps*dot(grad(u), grad(v))*dx - f*v*dx 
+    array = [w,eps]
     timeseries = TimeSeries('timeseries_cde')
     t = 0
     for n in range(1,num_steps+1):
     
     # Update current time
         t += dt
+        u_D.t = t 
         A = assemble(lhs(F))
         b = assemble(rhs(F))
         bc.apply(b)
 
     # Read velocity from file
-        timeseries.store(w.vector(), t)
+        # timeseries.store(w.vector(), t)
 
     # Solve variational problem for time step
         solve(F == 0, u,bc)
 
     # Update previous solution
         u_n.assign(u)
-        write(u,array,nx,ny,n,num_steps,sim_number)
-        # print(u.compute_vertex_values().shape,w.compute_vertex_values().shape)
-        plot(u)
-        plt.axis('off')
+        write_function_values(u,nx,ny,n,num_steps,sim_number)
+        # plot(u,mode='color')
+        # plt.axis('off')
         
-    # plt.savefig('results/64grid/'+str(sim_number)+'/'+str(n)+'.png')
-    # Update progress bar
-    # progress.update(t / T)   
-# Hold plot
+        # plt.savefig('data/'+str(sim_number)+'/'+str(n)+'.png')
+    # write_parameters(array,sim_number)  
 
 
-for i in range(1,51):
+for i in range(1):
     Simulation(64,64,10,100,i)
